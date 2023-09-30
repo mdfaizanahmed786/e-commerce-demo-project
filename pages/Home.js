131367137b662e86b8e6f1e720206ad463eb037f
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-09-30 11:19:38 +0530

    added a carousel file

--- pages/Home.js
+++ pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Carousel from '../components/Home/Carousel';
 
 function Home() {
-  return <div className="container"></div>;
+  return (
+    <Container>
+      <h2 className="mt-5">Welcome!</h2>
+      <Carousel />
+    </Container>
+  );
 }
 
 export default Home;
