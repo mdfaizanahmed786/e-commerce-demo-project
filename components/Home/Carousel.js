57d0721574425c9ff1b5f0969c7cd3ecb3f45c02
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-09-30 11:29:11 +0530

    1 file modified

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function Carousel() {
   const [index, setIndex] = useState(0);
@@ -12,21 +11,18 @@ function Carousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
