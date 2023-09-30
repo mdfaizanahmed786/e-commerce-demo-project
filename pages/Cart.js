995519cca43e14911d639b186f9b100238462c12
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-10-01 00:47:46 +0530

    ui: created cart page

--- pages/Cart.js
+++ pages/Cart.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 function Cart() {
   const cartItems = useSelector((state) => state.cart.items);
-  console.log(cartItems);
+
   return (
-    <div className="container">
-      <h1>Cart Page</h1>
-    </div>
+    <Container className="mt-5">
+      <h1 className="mb-4">Cart Page</h1>
+      <Row>
+        {cartItems.map((item) => (
+          <Col
+            key={item.id}
+            md={6}
+            lg={4}
+            className="mb-4 d-flex w-100 justify-content-center"
+          >
+            <Card
+              className="shadow"
+              style={{ width: '23rem', height: 'fit-content' }}
+            >
+              <div className="d-flex w-100  justify-content-center">
+                <Card.Img
+                  variant="top"
+                  src={item.image}
+                  alt={item.title}
+                  className="p-3"
+                  style={{ width: '12rem' }}
+                />
+              </div>
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <Button variant="outline-secondary">-</Button>
+                    <span className="mx-2 mt-2">{item.quantity}</span>
+                    <Button variant="outline-secondary">+</Button>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
