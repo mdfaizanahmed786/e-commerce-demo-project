ab27e371f709c59b74698edb250f22eda4c21477
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-09-30 14:00:36 +0530

    api: invoked api to get list of products

--- components/Home/Products.js
+++ components/Home/Products.js
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { Container, Row, Col, Card, Spinner, Button } from 'react-bootstrap';
+
 function Products() {
-  return <div>List of products</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => {
+        setLoading(false);
+        setProducts(json);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <h2 className="mt-5 mb-3">Our Products</h2>
+
+      {loading && (
+        <div className="d-flex justify-content-center align-items-center mt-5">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )}
+
+      <Row>
+        {products.slice(8, 16).map((product) => (
+          <Col key={product.id} md={4}>
+            <Card className="mt-4 shadow">
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>{product.description.slice(0, 200)}...</Card.Text>
+                <Card.Text style={{ fontWeight: 'bold' }}>
+                  ${product.price}
+                </Card.Text>
+                <Button variant="outline-primary">Add to cart</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default Products;
--- pages/Home.js
+++ pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Slider from '../components/Home/Slider';
+import Products from '../components/Home/Products';
 
 function Home() {
   return (
     <>
       <Slider />
 
-      <div></div>
+      <div>
+<Products/>
+      </div>
     </>
   );
 }
