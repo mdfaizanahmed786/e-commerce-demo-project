20b29d6b0c367ed623cf4fafa91fee6c3236ddc6
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-10-01 15:56:57 +0530

    1 file modified

--- components/Home/Slider.js
+++ components/Home/Slider.js
@@ -7,7 +7,7 @@ function Slider() {
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-flex  w-100 img-fluid"
-          src="https://images.pexels.com/photos/3769747/pexels-photo-3769747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/3823488/pexels-photo-3823488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="First slide"
           style={{
             objectFit: 'cover',
@@ -19,7 +19,7 @@ function Slider() {
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-block w-100 img-fluid"
-          src="https://images.pexels.com/photos/3760081/pexels-photo-3760081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/5264900/pexels-photo-5264900.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Second slide"
           style={{
             objectFit: 'cover',
@@ -31,7 +31,7 @@ function Slider() {
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-block w-100 img-fluid"
-          src="https://images.pexels.com/photos/3823488/pexels-photo-3823488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src="https://images.pexels.com/photos/3760081/pexels-photo-3760081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Third slide"
           style={{
             objectFit: 'cover',
