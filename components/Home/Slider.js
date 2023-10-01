133f8e2479c36450ef31be1fb7bc475b7ce8b583
mdfaizanahmed786 <85175130+mdfaizanahmed786@users.noreply.github.com>
2023-10-01 14:12:18 +0530

    ui: changed slider images

--- components/Home/Slider.js
+++ components/Home/Slider.js
@@ -3,51 +3,39 @@ import { Carousel } from 'react-bootstrap';
 
 function Slider() {
   return (
-    <Carousel fade className="p-3">
+    <Carousel fade>
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-flex  w-100 img-fluid"
-          src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+          src="https://images.pexels.com/photos/3769747/pexels-photo-3769747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="First slide"
           style={{
             objectFit: 'cover',
-            maxWidth: '350px',
           }}
         />
-        <Carousel.Caption>
-          <h3>T shirts</h3>
-          <p>Explore wide range of t-shirts</p>
-        </Carousel.Caption>
+       
       </Carousel.Item>
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-block w-100 img-fluid"
-          src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+          src="https://images.pexels.com/photos/3760081/pexels-photo-3760081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Second slide"
           style={{
             objectFit: 'cover',
-            maxWidth: '350px',
           }}
         />
-        <Carousel.Caption>
-          <h3 classsName="text-dark">Jackets</h3>
-          <p>Explore wide range of jackets</p>
-        </Carousel.Caption>
+      
       </Carousel.Item>
       <Carousel.Item className="d-flex justify-content-center">
         <img
           className="d-block w-100 img-fluid"
-          src="https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
+          src="https://images.pexels.com/photos/3823488/pexels-photo-3823488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Third slide"
           style={{
             objectFit: 'cover',
-            maxWidth: '350px',
           }}
         />
-        <Carousel.Caption>
-          <h3>Women wear</h3>
-          <p>Pick top collection from women wear</p>
-        </Carousel.Caption>
+       
       </Carousel.Item>
     </Carousel>
   );
